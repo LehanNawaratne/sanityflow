6a1880cb6c06d437bf2987d6bd91f9f44f886b40
LehanNawaratne <162142452+LehanNawaratne@users.noreply.github.com>
2026-02-18 10:16:06 +0530

    Delete backend/dist directory

--- backend/dist/utils/errorHandler.js
+++
@@ -1,9 +0,0 @@
-import { HTTP_STATUS } from '../constants/index.js';
-const errorHandler = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(err.status || HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-        error: err.message || 'Internal Server Error'
-    });
-};
-export default errorHandler;
-//# sourceMappingURL=errorHandler.js.map
