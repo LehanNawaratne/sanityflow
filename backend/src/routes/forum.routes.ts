45df635e8d732452a0827c62f798784e924ef7c8
Gaindu Nuhansith <136603140+gaindunuhansith@users.noreply.github.com>
2026-02-27 20:42:52 +0530

    feat: implement forum routes

---
+++ backend/src/routes/forum.routes.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import auth from '../middleware/auth.js';
+
+import {
+  getAllThreadsHandler,
+  getThreadByIdHandler,
+  createThreadHandler,
+  updateThreadHandler,
+  deleteThreadHandler,
+  getRepliesHandler,
+  createReplyHandler,
+  deleteReplyHandler,
+} from '../controllers/forum.controller.js';
+
+const forumRouter = Router();
+
+forumRouter.get('/', getAllThreadsHandler);
+forumRouter.post('/', auth, createThreadHandler);
+forumRouter.get('/:id', getThreadByIdHandler);
+forumRouter.patch('/:id', auth, updateThreadHandler);
+forumRouter.delete('/:id', auth, deleteThreadHandler);
+
+forumRouter.get('/:id/replies', getRepliesHandler);
+forumRouter.post('/:id/replies', auth, createReplyHandler);
+forumRouter.delete('/:id/replies/:replyId', auth, deleteReplyHandler);
+
+export default forumRouter;
