442786a1c612ad8fa9a58cd19e3bf48153b5498b
ImethLochanaWeerasooriya <IT23577138@my.sliit.lk>
2026-02-27 19:43:52 +0530

    feat: added the barcode controller file

---
+++ backend/src/controllers/barcode.controller.ts
@@ -0,0 +1,26 @@
+import type { Request, Response, NextFunction } from "express";
+import { HTTP_STATUS } from "../constants/index.js";
+import { barcodeSchema } from "../validations/barcode.validation.js";
+import { lookupBarcodeService } from "../services/barcode.service.js";
+
+export const lookupBarcodeController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const parsed = barcodeSchema.parse(req.params);
+
+    const product = await lookupBarcodeService(parsed.barcode);
+
+    if (!product) {
+      return res
+        .status(HTTP_STATUS.NOT_FOUND)
+        .json({ error: "Product not found in external database" });
+    }
+
+    res.status(HTTP_STATUS.OK).json(product);
+  } catch (error) {
+    next(error);
+  }
+};
